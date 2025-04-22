6da7c5bf6c3f39d1848f8fa04841383ef90941ea
Vĩnh Kỳyy <89082148+vinhky123@users.noreply.github.com>
2025-04-22 17:49:07 +0700

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,39 @@
+"""
+Problem: Write a function that takes a string and returns the first non-repeating character in the
+string. If there are no non-repeating characters, return null
+
+Example:
+Input: "sapstar"
+Output: "p"
+
+Approach: Use a for statement to loop thourgh all characters of the string to build a hashmap in order to find out which character are repeating.
+To handle this in Python, the data type called dictionary will be used.
+Finally, iterate through the string again to find the first character with a count of 1.
+"""
+
+
+def first_non_repeating(s: str):
+    # initialize the data structure
+    count_map = {}
+
+    # loop through the string to build a hashmap
+    for char in s:
+        if char in count_map:  # if it existed
+            count_map[char] += 1
+
+        else:  # if not
+            count_map[char] = 1
+
+    for char in s:  # iterate again
+        if count_map[char] == 1:  # if it is a non-repeating character
+            return char
+
+    return "null"  # When the second loop is over but no character returned
+
+
+if __name__ == "__main__":
+    input = str(input("Type Input: "))
+
+    output = first_non_repeating(input)
+
+    print(output)
